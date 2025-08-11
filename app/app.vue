fda2dd8d7a9282bedeaf6e9ccbcabf492e3dec74
Animesh <animesh.workplace@gmail.com>
2025-08-11 12:01:58 +0530

    added file - app/app.vue

---
+++ app/app.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const { colorMode } = useTheme();
+</script>
+<template>
+  <div
+    class="min-h-screen bg-light-bg text-light-text dark:bg-dark-bg dark:text-dark-text transition-colors"
+  >
+    <Navbar /> <NuxtPage /> <Footer />
+  </div>
+</template>
