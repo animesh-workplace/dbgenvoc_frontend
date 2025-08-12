e2beca07e4c01f91300bc245ca741be1e268a487
Animesh <animesh.workplace@gmail.com>
2025-08-12 18:40:28 +0530

    updated file - nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,27 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import Aura from '@primeuix/themes/aura'
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  modules: ['@nuxt/fonts', '@nuxt/icon', '@nuxtjs/tailwindcss']
+	devtools: { enabled: true },
+	compatibilityDate: '2025-07-15',
+	css: ['@/assets/css/tailwind.css'],
+	vite: { plugins: [tailwindcss()] },
+	googleFonts: {
+		preload: true,
+		prefetch: true,
+		useStylesheet: true,
+		families: { Poppins: true, 'Lexend+Deca': { wght: '100..900' } },
+		outputDir: 'app/assets',
+	},
+	modules: ['@nuxt/icon', '@primevue/nuxt-module', '@nuxtjs/google-fonts'],
+	icon: { clientBundle: { scan: true }, serverBundle: { collections: ['solar'] } },
+	primevue: {
+		options: {
+			ripple: true,
+			theme: { preset: Aura, options: { darkModeSelector: '.dark' } },
+		},
+		components: {
+			include: [],
+		},
+	},
 })
