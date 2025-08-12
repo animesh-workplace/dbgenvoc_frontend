1e884ccf8d97a193bce7555e42a7b53aee0623ee
Animesh <animesh.workplace@gmail.com>
2025-08-13 00:38:44 +0530

    updated file - app/components/Footer.vue

--- app/components/Footer.vue
+++ app/components/Footer.vue
@@ -1,14 +1,143 @@
 <template>
-  <footer class="mt-10 border-t border-light-border dark:border-dark-border">
-    <div
-      class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-6 text-sm text-light-muted dark:text-white/50 flex items-center justify-between"
-    >
-      <span>© {{ new Date().getFullYear() }} dbGENVOC</span>
-      <div class="flex gap-4"><a href="#">Docs</a> <a href="#">Contact</a></div>
-    </div>
-  </footer>
+	<footer class="bg-white dark:bg-slate-900">
+		<div class="mx-8">
+			<div class="lg:grid lg:grid-cols-2">
+				<!-- Section -->
+				<div
+					class="border-b border-gray-100 py-8 lg:order-last lg:border-s lg:border-b-0 lg:py-16 lg:p-16 dark:border-gray-800"
+				>
+					<div class="mt-8 space-y-4 lg:mt-0">
+						<div>
+							<!-- <h2 class="text-2xl font-medium text-gray-900 dark:text-white">About </h2> -->
+
+							<p class="mt-4 text-gray-500 dark:text-gray-400" align="justify">
+								dbGENVOC is a comprehensive, flexible database framework designed to provide
+								researchers with seamless access to somatic variation data from Indian oral cancer
+								patients.
+							</p>
+						</div>
+					</div>
+				</div>
+
+				<!-- Links -->
+				<div class="py-8 lg:py-16 lg:p-16">
+					<!-- Website Logo -->
+					<div class="hidden text-teal-600 lg:block dark:text-teal-300">
+						<div class="grid grid-cols-2">
+							<div class="flex justify-center items-center border-x border-gray-200 px-4">
+								<div class="text-2xl font-extrabold cursor-pointer text-white">dbGENVOC</div>
+								<div>
+									<button
+										class="rounded-2xl bg-gray-100/10 py-0.5 px-2 text-xs font-medium text-gray-50 cursor-pointer"
+									>
+										v2
+									</button>
+								</div>
+							</div>
+
+							<div class="flex justify-center items-center border-r border-gray-200 px-4">
+								<div class="text-2xl font-extrabold cursor-pointer text-white">BRIC NIBMG</div>
+							</div>
+						</div>
+					</div>
+
+					<!-- Website links -->
+					<div class="mt-8 grid grid-cols-1 gap-8 sm:grid-cols-3">
+						<div>
+							<p class="font-medium text-gray-900 dark:text-white">Site Link</p>
+
+							<ul class="mt-6 space-y-4 text-sm">
+								<li v-for="(link, index) in siteLinks" :key="index">
+									<NuxtLink
+										:to="link.href"
+										class="text-gray-700 transition hover:opacity-75 dark:text-gray-200"
+									>
+										{{ link.label }}
+									</NuxtLink>
+								</li>
+							</ul>
+						</div>
+
+						<!-- Helpful links -->
+						<div>
+							<p class="font-medium text-gray-900 dark:text-white">Helpful Links</p>
+
+							<ul class="mt-6 space-y-4 text-sm">
+								<li v-for="(link, index) in helpfulLinks" :key="index">
+									<NuxtLink
+										:to="link.href"
+										class="text-gray-700 transition hover:opacity-75 dark:text-gray-200"
+									>
+										{{ link.label }}
+									</NuxtLink>
+								</li>
+							</ul>
+						</div>
+
+						<!-- NIBMG Links -->
+						<div>
+							<p class="font-medium text-gray-900 dark:text-white">BRIC NIBMG</p>
+
+							<ul class="mt-6 space-y-4 text-sm">
+								<li v-for="(link, index) in nibmgLinks" :key="index">
+									<NuxtLink
+										:to="link.href"
+										class="text-gray-700 transition hover:opacity-75 dark:text-gray-200"
+									>
+										{{ link.label }}
+									</NuxtLink>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<!-- Final Links -->
+			<div class="border-t border-gray-100 py-8 dark:border-gray-800 text-center">
+				<ul class="flex flex-wrap gap-4 text-xs justify-center">
+					<li v-for="(link, index) in basic_links" :key="index">
+						<NuxtLink
+							:to="link.href"
+							class="text-gray-500 transition hover:opacity-75 dark:text-gray-400"
+						>
+							{{ link.label }}
+						</NuxtLink>
+					</li>
+				</ul>
+
+				<p class="mt-4 text-xs text-gray-500 dark:text-gray-400">
+					&copy; {{ new Date().getFullYear() }} | dbGENVOC | BRIC-NIBMG | All Rights Reserved
+				</p>
+			</div>
+		</div>
+	</footer>
 </template>
 
-<script setup></script>
+<script setup>
+const basic_links = [
+	{ label: 'Terms & Conditions', href: '#' },
+	{ label: 'Privacy Policy', href: '#' },
+	{ label: 'Cookies', href: '#' },
+]
 
-<style scoped></style>
+const siteLinks = [
+	{ label: 'Home', href: '/' },
+	{ label: 'Help', href: '/help' },
+	{ label: 'Contact us', href: '/contact' },
+	{ label: 'dbGENVOC v1', href: '#' },
+]
+
+const nibmgLinks = [
+	{ label: 'Home', href: 'https://www.nibmg.ac.in' },
+	{ label: 'Reach us', href: 'https://www.nibmg.ac.in/careers/' },
+	{ label: 'Contact us', href: 'https://www.nibmg.ac.in/p/contact' },
+	{ label: 'About BRIC', href: 'https://www.bric.nic.in' },
+]
+
+const helpfulLinks = [
+	{ label: 'Manuscript', href: 'https://doi.org/10.1093/database/baab034' },
+	{ label: 'Frontend Source Code', href: '#' },
+	{ label: 'Backend Source Code', href: '#' },
+]
+</script>
