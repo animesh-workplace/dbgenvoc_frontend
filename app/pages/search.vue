60551c097e598a2bfaf314ebf860093cd9b89410
Animesh <animesh.workplace@gmail.com>
2025-08-21 12:11:38 +0530

    updated file - app/pages/search.vue

--- app/pages/search.vue
+++ app/pages/search.vue
@@ -9,6 +9,29 @@
 	</div>
 </template>
 
-<script setup></script>
+<script setup>
+import { useGeneAPI } from '@/api/geneAPI'
+
+const searchVariantType = async () => {
+	const { getSearchAPI } = useGeneAPI()
+	try {
+		const response = await getSearchAPI('exome_somatic', {
+			term: 'TP53',
+			exact_match: true,
+			search_columns: ['gene'],
+		})
+		console.log(response)
+	} catch (error) {
+		console.error('Error fetching search data:', error)
+	}
+}
+
+onMounted(() => {
+	nextTick(async () => {
+		// Initialize the search variant type function
+		searchVariantType()
+	})
+})
+</script>
 
 <style scoped></style>
