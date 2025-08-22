504ab65ed9d6619d0779beed312183426a75577d
Animesh <animesh.workplace@gmail.com>
2025-08-22 10:12:04 +0530

    updated file - app/api/geneAPI.js

--- app/api/geneAPI.js
+++ app/api/geneAPI.js
@@ -3,7 +3,7 @@ export function useGeneAPI() {
 	// const BASEURL = `${config.public.API_BASE_URL}`
 	const BASEURL = 'http://10.10.6.80/dbgenvoc/api/v2'
 
-	const getSearchAPI = async (table_name, payload) => {
+	const SearchAPI = async (table_name, payload) => {
 		try {
 			const { data, error } = await useFetch(`${BASEURL}/${table_name}/search/`, {
 				method: 'POST',
@@ -21,7 +21,45 @@ export function useGeneAPI() {
 		}
 	}
 
+	const AggregateAPI = async (table_name, payload) => {
+		try {
+			const { data, error } = await useFetch(`${BASEURL}/${table_name}/aggregate/`, {
+				method: 'POST',
+				body: payload,
+			})
+
+			if (error.value) {
+				throw new Error(error.value || 'An error occurred')
+			}
+
+			return data.value
+		} catch (err) {
+			console.error(err)
+			throw err
+		}
+	}
+
+	const ConcateAggregateAPI = async (table_name, payload) => {
+		try {
+			const { data, error } = await useFetch(`${BASEURL}/${table_name}/aggregate-concatenated/`, {
+				method: 'POST',
+				body: payload,
+			})
+
+			if (error.value) {
+				throw new Error(error.value || 'An error occurred')
+			}
+
+			return data.value
+		} catch (err) {
+			console.error(err)
+			throw err
+		}
+	}
+
 	return {
-		getSearchAPI,
+		SearchAPI,
+		AggregateAPI,
+		ConcateAggregateAPI,
 	}
 }
