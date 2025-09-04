6bc7faf234cff425ce8785cb7c476ebea0e8e2ca
Animesh <animesh.workplace@gmail.com>
2025-09-04 20:05:22 +0530

    updated file - nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,24 @@
 import Aura from '@primeuix/themes/aura'
 import tailwindcss from '@tailwindcss/vite'
+import { definePreset } from '@primeuix/themes'
+
+const dbgenvocTheme = definePreset(Aura, {
+	semantic: {
+		primary: {
+			50: '{blue.50}',
+			100: '{blue.100}',
+			200: '{blue.200}',
+			300: '{blue.300}',
+			400: '{blue.400}',
+			500: '{blue.500}',
+			600: '{blue.600}',
+			700: '{blue.700}',
+			800: '{blue.800}',
+			900: '{blue.900}',
+			950: '{blue.950}',
+		},
+	},
+})
 
 export default defineNuxtConfig({
 	devServer: { port: 3011 },
@@ -28,7 +47,7 @@ export default defineNuxtConfig({
 	primevue: {
 		options: {
 			ripple: true,
-			theme: { preset: Aura, options: { darkModeSelector: '.dark' } },
+			theme: { preset: dbgenvocTheme, options: { darkModeSelector: '.dark' } },
 		},
 		components: { include: [] },
 	},
