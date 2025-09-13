e773e0342a374e4d5ebae062e1fca80ba32cad46
Animesh <animesh.workplace@gmail.com>
2025-09-13 18:34:38 +0530

    updated file - app/components/Result/SectionTCGA.vue

--- app/components/Result/SectionTCGA.vue
+++ app/components/Result/SectionTCGA.vue
@@ -8,67 +8,115 @@
 		</div>
 	</div>
 
-	<div class="p-8">
-		<DataTable
-			lazy
-			rowHover
-			paginator
-			scrollable
-			class="p-1"
-			stripedRows
-			size="small"
-			showGridlines
-			removableSort
-			:rows="noOfRows"
-			resizableColumns
-			@sort="HandleSort"
-			@page="HandlePage"
-			scrollHeight="20rem"
-			columnResizeMode="expand"
-			:value="searchData.results"
-			:totalRecords="searchData.total_results"
-			:rowsPerPageOptions="[50, 100, 200, 500]"
-		>
-			<Column v-for="col of columns" :key="col.field" :field="col.field" :header="col.header" sortable />
-		</DataTable>
-	</div>
+	<div v-if="!noData">
+		<div class="p-8">
+			<DataTable
+				lazy
+				rowHover
+				paginator
+				scrollable
+				class="p-1"
+				stripedRows
+				size="small"
+				showGridlines
+				removableSort
+				:rows="noOfRows"
+				resizableColumns
+				@sort="HandleSort"
+				@page="HandlePage"
+				scrollHeight="20rem"
+				columnResizeMode="expand"
+				:value="searchData.results"
+				:totalRecords="searchData.total_results"
+				:rowsPerPageOptions="[50, 100, 200, 500]"
+			>
+				<Column
+					sortable
+					:key="col.field"
+					:field="col.field"
+					:header="col.header"
+					v-for="col of columns.filter((c) => c.field !== 'reference_url')"
+				>
+					<template #body="slotProps">
+						<!-- Handling reference URL -->
+						<template v-if="col.field === 'reference'">
+							<NuxtLink
+								target="_blank"
+								:to="slotProps.data.reference_url"
+								class="text-blue-600 hover:underline"
+							>
+								{{ slotProps.data.reference }}
+							</NuxtLink>
+						</template>
 
-	<div class="px-8 pt-8">
-		<div
-			class="p-4 mb-2 text-lg text-blue-900 rounded-2xl text-center bg-sky-50 flex justify-center items-center gap-2"
-		>
-			<Icon name="tabler:graph-filled" class="!w-5 !h-5" />
-			<span class="font-semibold">Graphical Representation of TCGA variations</span>
+						<!-- Handling Entrez Gene ID -->
+						<template v-else-if="col.field === 'entrez_gene_id'">
+							<NuxtLink
+								target="_blank"
+								:to="`https://www.ncbi.nlm.nih.gov/gene/${slotProps.data.entrez_gene_id}`"
+								class="text-blue-600 hover:underline"
+							>
+								{{ slotProps.data.entrez_gene_id }}
+							</NuxtLink>
+						</template>
+
+						<!-- Handling UCSC Browser -->
+						<template v-else-if="col.field === 'genome_change_link'">
+							<NuxtLink
+								target="_blank"
+								:to="generateUCSCUrl(slotProps.data.genome_change)"
+								class="text-blue-600 hover:underline"
+							>
+								View in UCSC
+							</NuxtLink>
+						</template>
+
+						<!-- Default case for other columns -->
+						<template v-else>
+							{{ slotProps.data[col.field] }}
+						</template>
+					</template>
+				</Column>
+			</DataTable>
 		</div>
-	</div>
 
-	<div
-		:index="index"
-		v-for="(disease, index) in diseaseList"
-		class="grid lg:grid-cols-3 gap-8 p-8 md:grid-cols-2 grid-cols-1"
-	>
-		<div class="text-center">
-			<div class="pb-2 text-sm font-semibold text-gray-700">
-				Variant Classification Coding ({{ disease }})
+		<div class="px-8 pt-8">
+			<div
+				class="p-4 mb-2 text-lg text-blue-900 rounded-2xl text-center bg-sky-50 flex justify-center items-center gap-2"
+			>
+				<Icon name="tabler:graph-filled" class="!w-5 !h-5" />
+				<span class="font-semibold">Graphical Representation of TCGA variations</span>
 			</div>
-			<GraphBar :plotData="variantClass[disease]" showAll horizontal />
 		</div>
 
-		<div class="text-center">
-			<div class="pb-2 text-sm font-semibold text-gray-700">Variant Type ({{ disease }})</div>
-			<GraphBar :plotData="variantType[disease]" />
-		</div>
+		<div
+			:index="index"
+			v-for="(disease, index) in diseaseList"
+			class="grid lg:grid-cols-3 gap-8 p-8 md:grid-cols-2 grid-cols-1"
+		>
+			<div class="text-center">
+				<div class="pb-2 text-sm font-semibold text-gray-700">
+					Variant Classification Coding ({{ disease }})
+				</div>
+				<GraphBar :plotData="variantClass[disease]" showAll horizontal />
+			</div>
 
-		<div class="text-center">
-			<div class="pb-2 text-sm font-semibold text-gray-700">SNV Class ({{ disease }})</div>
-			<GraphBar :plotData="snvClass[disease]" showAll />
+			<div class="text-center">
+				<div class="pb-2 text-sm font-semibold text-gray-700">Variant Type ({{ disease }})</div>
+				<GraphBar :plotData="variantType[disease]" />
+			</div>
+
+			<div class="text-center">
+				<div class="pb-2 text-sm font-semibold text-gray-700">SNV Class ({{ disease }})</div>
+				<GraphBar :plotData="snvClass[disease]" showAll />
+			</div>
+			<!-- <GraphLollipop class="col-span-3" /> -->
 		</div>
-		<!-- <GraphLollipop class="col-span-3" /> -->
 	</div>
 </template>
 
 <script setup>
-import { uniq, map, forEach } from 'lodash-es'
+import { uniq, map } from 'lodash-es'
 const { useVariantMatrix } = useHelper()
 import { useGeneAPI } from '@/api/geneAPI'
 const { SearchAPI, AggregateAPI, ConcateAggregateAPI } = useGeneAPI()
@@ -100,6 +148,7 @@ const columns = [
 	{ field: 'start', header: 'Start' },
 	{ field: 'end', header: 'End' },
 	{ field: 'genome_change', header: 'Genome Change' },
+	{ field: 'genome_change_link', header: 'UCSC Browser' },
 	{ field: 'cDNA_change', header: 'cDNA Change' },
 	{ field: 'codon_change', header: 'Codon Change' },
 	{ field: 'protein_change', header: 'Protein Change' },
@@ -118,7 +167,62 @@ const columns = [
 
 const props = defineProps({
 	tableName: { type: String, default: '' },
+	noData: { type: Boolean, default: false },
 })
+
+const parseGenomicCoordinates = (genomeChange) => {
+	// Patterns for different variant types:
+	// 1. SNVs: g.chr13:32910626C>T, chr13:32910626C>T
+	// 2. Deletions: g.chr17:7577092delC, chr17:7577092delC
+	// 3. Insertions: g.chr1:12345insA, chr1:12345insA
+	// 4. Simple coordinates: g.chr13:32910626, chr13:32910626
+
+	const patterns = [
+		// SNVs with g. prefix
+		/g\.(chr[XY\d]+):(\d+)([ACGT]>[ACGT])/i,
+		// Deletions with g. prefix
+		/g\.(chr[XY\d]+):(\d+)del([ACGT]+)/i,
+		// Insertions with g. prefix
+		/g\.(chr[XY\d]+):(\d+)ins([ACGT]+)/i,
+		// Simple coordinates with g. prefix
+		/g\.(chr[XY\d]+):(\d+)/i,
+
+		// Same patterns without g. prefix
+		/(chr[XY\d]+):(\d+)([ACGT]>[ACGT])/i,
+		/(chr[XY\d]+):(\d+)del([ACGT]+)/i,
+		/(chr[XY\d]+):(\d+)ins([ACGT]+)/i,
+		/(chr[XY\d]+):(\d+)/i,
+	]
+
+	for (const pattern of patterns) {
+		const match = genomeChange.match(pattern)
+		if (match) {
+			const chromosome = match[1]
+			const position = parseInt(match[2])
+
+			// For UCSC Genome Browser, we typically show a small region around the variant
+			const start = Math.max(1, position - 50) // 50bp upstream
+			const end = position + 50 // 50bp downstream
+
+			return {
+				chromosome,
+				position,
+				start,
+				end,
+				formattedPosition: `${chromosome}:${start}-${end}`,
+			}
+		}
+	}
+	return null
+}
+
+// Function to generate UCSC Genome Browser URL
+const generateUCSCUrl = (genomeChange) => {
+	const coords = parseGenomicCoordinates(genomeChange)
+	if (!coords) return '#'
+
+	return `https://genome.ucsc.edu/cgi-bin/hgTracks?db=hg19&lastVirtModeType=default&lastVirtModeExtraState=&virtModeType=default&virtMode=0&nonVirtPosition=&position=${coords.formattedPosition}&hgsid=783385835_z0061mD0u0xo6HFQCdLHaeOZp9UA`
+}
 
 const HandleSort = async (event) => {
 	let sortOrder = 'asc'
@@ -246,6 +350,19 @@ const aggregateSNVClass = async (disease) => {
 		console.error('Error fetching search data:', error)
 	}
 }
+
+watch(
+	() => props.tableName,
+	() => {
+		nextTick(async () => {
+			searchVariantType()
+			aggregateSNVClass()
+			aggregateVariantType()
+			aggregateVariantClass()
+		})
+	},
+	{ immediate: true }, // This will run the watcher callback immediately
+)
 
 onBeforeMount(() => {
 	nextTick(async () => {
