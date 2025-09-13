3b1209d7acc3250bc7dd3818f3e8933be624f841
Animesh <animesh.workplace@gmail.com>
2025-09-13 20:14:31 +0530

    updated file - app/api/geneAPI.js

--- app/api/geneAPI.js
+++ app/api/geneAPI.js
@@ -3,6 +3,24 @@ export function useGeneAPI() {
 	// const BASEURL = `${config.public.API_BASE_URL}`
 	const BASEURL = 'http://10.10.6.80/dbgenvoc/api/v2'
 
+	const AutocompleteAPI = async (payload) => {
+		try {
+			const { data, error } = await useFetch(`${BASEURL}/autocomplete/`, {
+				method: 'POST',
+				body: payload,
+			})
+
+			if (error.value) {
+				throw new Error(error.value || 'An error occurred')
+			}
+
+			return data.value
+		} catch (err) {
+			console.error(err)
+			throw err
+		}
+	}
+
 	const SearchAPI = async (table_name, payload) => {
 		try {
 			const { data, error } = await useFetch(`${BASEURL}/${table_name}/search/`, {
@@ -60,6 +78,7 @@ export function useGeneAPI() {
 	return {
 		SearchAPI,
 		AggregateAPI,
+		AutocompleteAPI,
 		ConcateAggregateAPI,
 	}
 }
