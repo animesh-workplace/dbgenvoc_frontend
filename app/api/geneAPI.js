fe72087cd78f058d655e0d7129c634e9b025b4d2
Animesh <animesh.workplace@gmail.com>
2025-10-17 12:21:26 +0530

    updated file - app/api/geneAPI.js

--- app/api/geneAPI.js
+++ app/api/geneAPI.js
@@ -75,7 +75,26 @@ export function useGeneAPI() {
 		}
 	}
 
+	const AskAIAPI = async (payload) => {
+		try {
+			const { data, error } = await useFetch(`${BASEURL}/ask/`, {
+				method: 'GET',
+				query: payload,
+			})
+
+			if (error.value) {
+				throw new Error(error.value || 'An error occurred')
+			}
+
+			return data.value
+		} catch (err) {
+			console.error(err)
+			throw err
+		}
+	}
+
 	return {
+		AskAIAPI,
 		SearchAPI,
 		AggregateAPI,
 		AutocompleteAPI,
