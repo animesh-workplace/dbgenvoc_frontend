aaaf762f8c628627328cb4a1c48cae1245e9c9b9
Animesh <animesh.workplace@gmail.com>
2025-10-23 20:32:19 +0530

    added file - app/pages/test.vue

---
+++ app/pages/test.vue
@@ -0,0 +1,11 @@
+<template>
+	<div class="min-h-[calc(100vh-56px)]">
+		<div class="px-8 pt-8 max-w-4xl mx-auto">
+			<GraphHeatmap />
+		</div>
+	</div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
