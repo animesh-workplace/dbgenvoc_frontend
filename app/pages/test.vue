d413e09b125cf76a2754d46591c1a0373061f4c1
Animesh <animesh.workplace@gmail.com>
2025-10-24 11:25:52 +0530

    updated file - app/pages/test.vue

--- app/pages/test.vue
+++ app/pages/test.vue
@@ -1,7 +1,11 @@
 <template>
 	<div class="min-h-[calc(100vh-56px)]">
-		<div class="px-8 pt-8 max-w-4xl mx-auto">
-			<GraphHeatmap />
+		<div class="px-8 pt-8">
+			<!-- <Search class="mt-8" /> -->
+			<div class="grid grid-cols-2 gap-6">
+				<GraphHeatmap name="TCGA exome oncoplot" />
+				<GraphHeatmap name="NIBMG exome somatic oncoplot" />
+			</div>
 		</div>
 	</div>
 </template>
