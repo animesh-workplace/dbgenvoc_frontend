90ceeb08ddb90bf3c4be6d8255850df8d8ff16d2
Animesh <animesh.workplace@gmail.com>
2025-11-21 13:34:01 +0530

    updated file - app/pages/test.vue

--- app/pages/test.vue
+++ app/pages/test.vue
@@ -2,18 +2,21 @@
 	<div class="min-h-[calc(100vh-56px)] my-12">
 		<div class="px-8 pt-8">
 			<!-- <Search class="mt-8" /> -->
-			<div class="grid grid-cols-2 gap-6 my-8">
+			<!-- <div class="grid grid-cols-2 gap-6 my-8">
 				<GraphOncoplot name="TCGA exome oncoplot" />
 				<GraphOncoplot name="NIBMG exome somatic oncoplot" />
-			</div>
-			<div class="grid grid-cols-2 gap-6 my-8">
+			</div> -->
+			<!-- <div class="grid grid-cols-2 gap-6 my-8">
 				<GraphMatrix />
 				<GraphMatrix />
-			</div>
+			</div> -->
 			<!-- <div class="grid grid-cols-2 gap-6 my-8">
 				<GraphSomaticInteraction name="TCGA exome oncoplot" />
 				<GraphSomaticInteraction name="NIBMG exome somatic oncoplot" />
 			</div> -->
+			<div class="grid grid-cols-1 gap-6 my-8">
+				<GraphLollipop />
+			</div>
 		</div>
 	</div>
 </template>
