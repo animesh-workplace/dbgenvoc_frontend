2850854fda979fc516ff562e9406c3d23fec4027
Animesh <animesh.workplace@gmail.com>
2025-12-12 14:44:53 +0530

    updated file - app/components/Result/SectionMain.vue

--- app/components/Result/SectionMain.vue
+++ app/components/Result/SectionMain.vue
@@ -8,8 +8,9 @@
 		</div>
 	</div>
 
-	<div class="grid lg:grid-cols-3 gap-8 p-8 md:grid-cols-2 grid-cols-1">
-		<div class="col-span-3">
+	<!-- <div class="grid lg:grid-cols-4 gap-8 p-8 md:grid-cols-2 grid-cols-1"> -->
+	<div>
+		<div class="p-8">
 			<DataTable
 				lazy
 				rowHover
@@ -81,7 +82,7 @@
 			</DataTable>
 		</div>
 
-		<div class="pt-8 col-span-3">
+		<div class="px-8 pt-8">
 			<div
 				class="p-4 mb-2 text-lg text-blue-900 rounded-2xl text-center bg-sky-50 flex justify-center items-center gap-2"
 			>
@@ -90,41 +91,93 @@
 			</div>
 		</div>
 
-		<div class="text-right col-span-3">
-			<ToggleButton v-model="checked" onLabel="On" offLabel="Off" />
+		<div class="px-8 pt-8">
+			<div class="flex flex-wrap justify-center gap-4">
+				<button
+					:key="gene"
+					@click="handleLegendClick(gene)"
+					v-for="(gene, index) in genesList"
+					:style="{ backgroundColor: color_scheme[index] + '33' }"
+					class="flex items-center space-x-2 px-3 py-1 rounded-full text-sm transition-all border"
+					:class="
+						!hiddenSeries.includes(gene)
+							? 'border-gray-200 shadow-sm hover:shadow-md cursor-pointer'
+							: 'border-gray-200 opacity-70 inset-shadow-sm hover:inset-shadow-md cursor-pointer'
+					"
+				>
+					<span class="w-3 h-3 rounded-full" :style="{ backgroundColor: color_scheme[index] }"></span>
+					<span class="font-medium text-gray-700">{{ gene }}</span>
+				</button>
+			</div>
+			<div class="text-right col-span-3">
+				<div class="inline-flex items-center">
+					<span class="mr-2 text-sm font-medium text-gray-700">Show in %</span>
+					<ToggleSwitch v-model="percentageSwitcher" />
+				</div>
+			</div>
 		</div>
 
-		<div class="text-center">
-			<div class="pb-2 text-sm font-semibold text-gray-700">Variant Classification Coding</div>
-			<GraphBar :plotData="variantClass" showAll horizontal />
-		</div>
+		<div
+			class="grid gap-8 p-8 md:grid-cols-2 grid-cols-1"
+			:class="variantClassNoncoding?.categories?.length > 0 ? 'lg:grid-cols-4' : 'lg:grid-cols-3'"
+		>
+			<div class="text-center">
+				<div class="pb-2 text-sm font-semibold text-gray-700">Variant Classification Coding</div>
+				<GraphBar
+					showAll
+					horizontal
+					ref="graph1"
+					:plotData="variantClassCoding"
+					:isPercent="percentageSwitcher"
+				/>
+			</div>
 
-		<div class="text-center">
-			<div class="pb-2 text-sm font-semibold text-gray-700">Variant Type</div>
-			<GraphBar :plotData="variantType" />
-		</div>
+			<div class="text-center" v-if="variantClassNoncoding.categories.length > 0">
+				<div class="pb-2 text-sm font-semibold text-gray-700">Variant Classification Non-Coding</div>
+				<GraphBar
+					showAll
+					horizontal
+					ref="graph2"
+					:isPercent="percentageSwitcher"
+					:plotData="variantClassNoncoding"
+				/>
+			</div>
 
-		<div class="text-center">
-			<div class="pb-2 text-sm font-semibold text-gray-700">SNV Class</div>
-			<GraphBar :plotData="snvClass" showAll />
+			<div class="text-center">
+				<div class="pb-2 text-sm font-semibold text-gray-700">Variant Type</div>
+				<GraphBar ref="graph3" :plotData="variantType" :isPercent="percentageSwitcher" />
+			</div>
+
+			<div class="text-center">
+				<div class="pb-2 text-sm font-semibold text-gray-700">SNV Class</div>
+				<GraphBar ref="graph4" :plotData="snvClass" showAll :isPercent="percentageSwitcher" />
+			</div>
+
+			<!-- <GraphLollipop class="col-span-4" /> -->
 		</div>
-		<!-- <GraphLollipop class="col-span-3" /> -->
 	</div>
 </template>
 
 <script setup>
 import { uniq, map } from 'lodash-es'
-const { useVariantMatrix } = useHelper()
+const { useVariantMatrix, color_scheme } = useHelper()
 import { useGeneAPI } from '@/api/geneAPI'
 const { SearchAPI, AggregateAPI, ConcateAggregateAPI } = useGeneAPI()
 
+const graph1 = ref(null)
+const graph2 = ref(null)
+const graph3 = ref(null)
+const graph4 = ref(null)
 const noOfRows = ref(50)
 const route = useRoute()
-const searchData = ref({})
 const genesList = ref([])
-const snvClass = ref({ data: [], categories: [] })
-const variantType = ref({ data: [], categories: [] })
-const variantClass = ref({ data: [], categories: [] })
+const searchData = ref({})
+const hiddenSeries = ref([])
+const percentageSwitcher = ref(false)
+const snvClass = ref({ data: [], categories: [], total: 0 })
+const variantType = ref({ data: [], categories: [], total: 0 })
+const variantClassCoding = ref({ data: [], categories: [], total: 0 })
+const variantClassNoncoding = ref({ data: [], categories: [], total: 0 })
 const columns = [
 	{ field: 'variant_id', header: 'DB ID', frozen: false },
 	{ field: 'tumor_sample_barcode', header: 'Patient ID', frozen: true },
@@ -153,6 +206,22 @@ const columns = [
 const props = defineProps({
 	tableName: { type: String, default: '' },
 })
+
+const handleLegendClick = (seriesName) => {
+	// 1. Toggle UI state (for greying out the button)
+	if (hiddenSeries.value.includes(seriesName)) {
+		hiddenSeries.value = hiddenSeries.value.filter((n) => n !== seriesName)
+	} else {
+		hiddenSeries.value.push(seriesName)
+	}
+
+	// 2. Trigger the toggle in all child charts
+	// We use optional chaining (?.) just in case a chart isn't mounted yet
+	graph1.value?.toggleSeries(seriesName)
+	graph2.value?.toggleSeries(seriesName)
+	graph3.value?.toggleSeries(seriesName)
+	graph4.value?.toggleSeries(seriesName)
+}
 
 const parseGenomicCoordinates = (genomeChange) => {
 	// Patterns for different variant types:
@@ -245,8 +314,8 @@ const aggregateVariantType = async () => {
 	try {
 		const response = await AggregateAPI(props.tableName, {
 			column: 'variant_type',
-			aggregation_type: 'count',
 			group_by: ['variant_type', 'gene'],
+			aggregation_type: percentageSwitcher.value ? 'percentage' : 'count',
 			filters: { logic: 'AND', conditions: [{ column: 'gene', operator: 'in', value: genesList.value }] },
 		})
 
@@ -261,31 +330,74 @@ const aggregateVariantType = async () => {
 		)
 		variantType.value.data = matrix
 		variantType.value.categories = sort_row_names
+		variantType.value.total = response.total_records
 	} catch (error) {
 		console.error('Error fetching search data:', error)
 	}
 }
 
-const aggregateVariantClass = async () => {
+const aggregateVariantClass = async (disease) => {
+	let coding = [
+		'Silent',
+		'Splice_Site',
+		'In_Frame_Ins',
+		'In_Frame_Del',
+		'Stop_Codon_Ins',
+		'Stop_Codon_Del',
+		'Frame_Shift_Del',
+		'Frame_Shift_Ins',
+		'Start_Codon_SNP',
+		'Start_Codon_Ins',
+		'Start_Codon_Del',
+		'Nonstop_Mutation',
+		'Missense_Mutation',
+		'Nonsense_Mutation',
+		'De_novo_Start_InFrame',
+		'De_novo_Start_OutOfFrame',
+	]
+	let non_coding = ['IGR', 'Intron', "3'UTR", "5'Flank", "5'UTR", 'ncRNA']
+
 	let variant_categories = []
+
 	try {
 		const response = await AggregateAPI(props.tableName, {
 			column: 'variant_class',
-			aggregation_type: 'percentage',
 			group_by: ['variant_class', 'gene'],
-			filters: { logic: 'AND', conditions: [{ column: 'gene', operator: 'in', value: genesList.value }] },
+			aggregation_type: percentageSwitcher.value ? 'percentage' : 'count',
+			filters: {
+				logic: 'AND',
+				conditions: [{ column: 'gene', operator: 'in', value: genesList.value }],
+			},
 		})
 		variant_categories = uniq(map(response.result, (item) => item.variant_class))
+		const coding_categories = coding.filter((d) => variant_categories.includes(d))
+		const noncoding_categories = non_coding.filter((d) => variant_categories.includes(d))
 
-		const { matrix, sort_row_names } = useVariantMatrix(
+		const { matrix: coding_matrix, sort_row_names: coding_sort_row_names } = useVariantMatrix(
 			response.result,
-			variant_categories,
+			coding_categories,
 			genesList.value,
 			'variant_class',
 			true,
 		)
-		variantClass.value.data = matrix
-		variantClass.value.categories = map(sort_row_names, (d) =>
+
+		variantClassCoding.value.data = coding_matrix
+		variantClassCoding.value.total = response.total_records
+		variantClassCoding.value.categories = map(coding_sort_row_names, (d) =>
+			d.replaceAll('_', ' ').replaceAll('Mutation', ''),
+		)
+
+		const { matrix: non_coding_matrix, sort_row_names: non_coding_sort_row_names } = useVariantMatrix(
+			response.result,
+			noncoding_categories,
+			genesList.value,
+			'variant_class',
+			true,
+		)
+
+		variantClassNoncoding.value.data = non_coding_matrix
+		variantClassNoncoding.value.total = response.total_records
+		variantClassNoncoding.value.categories = map(non_coding_sort_row_names, (d) =>
 			d.replaceAll('_', ' ').replaceAll('Mutation', ''),
 		)
 	} catch (error) {
@@ -293,14 +405,42 @@ const aggregateVariantClass = async () => {
 	}
 }
 
+// const aggregateVariantClass = async () => {
+// 	let variant_categories = []
+// 	try {
+// 		const response = await AggregateAPI(props.tableName, {
+// 			column: 'variant_class',
+// 			group_by: ['variant_class', 'gene'],
+// 			aggregation_type: percentageSwitcher.value ? 'percentage' : 'count',
+// 			filters: { logic: 'AND', conditions: [{ column: 'gene', operator: 'in', value: genesList.value }] },
+// 		})
+// 		variant_categories = uniq(map(response.result, (item) => item.variant_class))
+
+// 		const { matrix, sort_row_names } = useVariantMatrix(
+// 			response.result,
+// 			variant_categories,
+// 			genesList.value,
+// 			'variant_class',
+// 			true,
+// 		)
+// 		variantClass.value.data = matrix
+// 		variantClass.value.categories = map(sort_row_names, (d) =>
+// 			d.replaceAll('_', ' ').replaceAll('Mutation', ''),
+// 		)
+// 		variantClass.value.total = response.total_records
+// 	} catch (error) {
+// 		console.error('Error fetching search data:', error)
+// 	}
+// }
+
 const aggregateSNVClass = async () => {
 	let variant_categories = []
 	try {
 		const response = await ConcateAggregateAPI(props.tableName, {
 			separator: '>',
 			group_by: ['gene'],
-			aggregation_type: 'count',
 			columns: ['ref_allele', 'tumor_seq_allele2'],
+			aggregation_type: percentageSwitcher.value ? 'percentage' : 'count',
 			filters: {
 				logic: 'AND',
 				conditions: [
@@ -319,6 +459,7 @@ const aggregateSNVClass = async () => {
 		)
 		snvClass.value.data = matrix
 		snvClass.value.categories = sort_row_names
+		snvClass.value.total = response.total_records
 	} catch (error) {
 		console.error('Error fetching search data:', error)
 	}
@@ -337,9 +478,22 @@ watch(
 	{ immediate: true }, // This will run the watcher callback immediately
 )
 
+watch(
+	() => percentageSwitcher.value,
+	() => {
+		nextTick(async () => {
+			searchVariantType()
+			aggregateSNVClass()
+			aggregateVariantType()
+			aggregateVariantClass()
+		})
+	},
+	{ immediate: true }, // This will run the watcher callback immediately
+)
+
 onMounted(() => {
 	nextTick(async () => {
-		genesList.value = route.query.genes_list
+		genesList.value = [].concat(route.query.genes_list || [])
 		// Initialize the search variant type function
 		searchVariantType()
 		aggregateSNVClass()
