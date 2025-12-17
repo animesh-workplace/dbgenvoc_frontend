7e379809e6d44c3191a143182bca80116f5b1f9e
Animesh <animesh.workplace@gmail.com>
2025-12-17 10:30:06 +0530

    updated file - app/app.vue

--- app/app.vue
+++ app/app.vue
@@ -1,9 +1,30 @@
 <script setup>
 const { colorMode } = useTheme()
+
+const isLoading = useGlobalLoader()
+
+// Optional: Hook into Nuxt routing to auto-start/stop
+const nuxtApp = useNuxtApp()
+nuxtApp.hook('page:start', () => {
+	isLoading.value = true
+})
+nuxtApp.hook('page:finish', () => {
+	isLoading.value = false
+})
 </script>
 
 <template>
 	<div class="min-h-screen bg-light-bg text-light-text dark:bg-dark-bg dark:text-dark-text transition-colors">
+		<div
+			v-if="isLoading"
+			class="fixed inset-0 z-[9999] bg-white/80 backdrop-blur-sm flex items-center justify-center"
+		>
+			<div class="flex flex-col items-center">
+				<!-- <Icon name="line-md:loading-loop" class="w-20 h-20 text-blue-600 mb-4" /> -->
+				<h2 class="text-xl font-bold text-gray-800">Searching Database...</h2>
+			</div>
+		</div>
+
 		<NuxtLayout>
 			<NuxtPage />
 		</NuxtLayout>
